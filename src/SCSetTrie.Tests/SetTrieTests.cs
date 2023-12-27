4cf3b4165c61dd0ea1f9bb006f67d78b491b7e65
Simon Condon <simoncondon@hotmail.com>
2023-12-27 18:32:44 +0000

    added some initial tests

--- src/SCSetTrie.Tests/SetTrieTests.cs
+++ src/SCSetTrie.Tests/SetTrieTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using FlUnit;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SCSetTrie.Tests
 {
@@ -9,6 +10,48 @@ namespace SCSetTrie.Tests
         public static Test GetSubsetsBehaviour => TestThat
             .GivenEachOf(() => new LookupTestCase[]
             {
+                new( // Empty trie
+                    Content: new HashSet<int>[] { },
+                    Query: new() { 1 },
+                    ExpectedResults: Enumerable.Empty<HashSet<int>>()),
+
+                new( // Smoke
+                    Content: new HashSet<int>[]
+                    {
+                        new() { },
+                        new() { 1 },
+                        new() { 1, 2 },
+                    },
+                    Query: new() { 1 },
+                    ExpectedResults: new HashSet<int>[]
+                    {
+                        new() { },
+                        new() { 1 },
+                    }),
+
+                new( // search for empty set - no match
+                    Content: new HashSet<int>[]
+                    {
+                        new() { 1 },
+                        new() { 1, 2 },
+                    },
+                    Query: new() { },
+                    ExpectedResults: new HashSet<int>[]
+                    {
+                    }),
+
+                new( // search for empty set - with match
+                    Content: new HashSet<int>[]
+                    {
+                        new() { },
+                        new() { 1 },
+                        new() { 1, 2 },
+                    },
+                    Query: new() { },
+                    ExpectedResults: new HashSet<int>[]
+                    {
+                        new() { }
+                    }),
             })
             .When(tc =>
             {
@@ -20,6 +63,50 @@ namespace SCSetTrie.Tests
         public static Test GetSupersetsBehaviour => TestThat
             .GivenEachOf(() => new LookupTestCase[]
             {
+                new( // Empty trie
+                    Content: new HashSet<int>[] { },
+                    Query: new() { 1 },
+                    ExpectedResults: Enumerable.Empty<HashSet<int>>()),
+
+                new( // Smoke
+                    Content: new HashSet<int>[]
+                    {
+                        new() { },
+                        new() { 1 },
+                        new() { 1, 2 },
+                    },
+                    Query: new() { 1 },
+                    ExpectedResults: new HashSet<int>[]
+                    {
+                        new() { 1 },
+                        new() { 1, 2 },
+                    }),
+
+                new( // search for empty set - without exact match
+                    Content: new HashSet<int>[]
+                    {
+                        new() { 1 },
+                        new() { 1, 2 },
+                    },
+                    Query: new() { },
+                    ExpectedResults: new HashSet<int>[]
+                    {
+                        new() { 1 },
+                        new() { 1, 2 },
+                    }),
+
+                new( // search for empty set - with exact match
+                    Content: new HashSet<int>[]
+                    {
+                        new() { },
+                        new() { 1 },
+                    },
+                    Query: new() { },
+                    ExpectedResults: new HashSet<int>[]
+                    {
+                        new() { },
+                        new() { 1 },
+                    }),
             })
             .When(tc =>
             {
@@ -28,13 +115,9 @@ namespace SCSetTrie.Tests
             })
             .ThenReturns((tc, rv) => rv.Should().BeEquivalentTo(tc.ExpectedResults));
 
-        private class LookupTestCase
-        {
-            public IEnumerable<HashSet<int>> Content { get; init; }
-
-            public HashSet<int> Query { get; init; }
-
-            public IEnumerable<HashSet<int>> ExpectedResults { get; init; }
-        }
+        private record LookupTestCase(
+            IEnumerable<HashSet<int>> Content,
+            HashSet<int> Query,
+            IEnumerable<HashSet<int>> ExpectedResults);
     }
 }
